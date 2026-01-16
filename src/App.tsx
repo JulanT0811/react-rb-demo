bf272dfc51d44bce3b65c174ffbfa13686df70a7
JulanT0811 <jtlengua.9b@gmail.com>
2026-01-15 21:35:03 -0500

    clase 13/01/2026

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import RBHero from './components/rb/RBHero'
+import RBCarousel from './components/rb/RBCarousel'
+import RBCardGrid from './components/rb/RBCardGrid'
+import RBNewsletter from './components/rb/RBNewsletter'
+
+function App() {
+
+  return (
+    <>
+      <RBHero/>
+      <RBCarousel/>
+      <RBCardGrid/>
+      <RBNewsletter/>
+    </>
+  )
+}
+
+export default App
